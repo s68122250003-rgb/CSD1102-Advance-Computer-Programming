e48db34b92ae32c4909dce136439dd3d78bd9d3f
s68122250003-rgb <s68122250003@ssru.ac.th>
2026-02-12 10:07:05 +0700

    Add files via upload

---
+++ program1/TestCircleRectangle1.java
@@ -0,0 +1,24 @@
+
+public class TestCircleRectangle1 {
+
+    public static void main(String[] args) {
+        Circle1 circle = new Circle1(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        Rectangle1 rectangle = new Rectangle1(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " + rectangle.getPerimeter());
+
+        Pentagon pentagon = new Pentagon(5, 2);
+        System.out.println("\nA pentagon " + pentagon.toString());
+        System.out.println("The area is " + pentagon.getArea());
+        System.out.println("The perimeter is " + pentagon.getPerimeter());
+    }
+
+}
+
